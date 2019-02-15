759c5158b463b99d42f8ca9f1871d030fb739293
FernandoCapporal <Luisferenc@ciencias.unam.mx>
2019-02-14 23:27:03 -0600

    Programas que imprimen los primeros N numeros (primos y naturalres)

---
+++ Primeros10Numeros.py
@@ -0,0 +1,5 @@
+print('Numeros 1 al 10:')
+x=1
+while(x<=10):
+        print (x)
+        x=x+1
---
+++ PrimerosNumeros.py
@@ -0,0 +1,7 @@
+n = int(input('Numeros:'))
+print("Numeros:")
+x=0
+for i in range(1,n+1):
+    print (i,end=",")
+    x=x+i
+print ("\nsuma",x)
